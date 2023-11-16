5c82426812e7110092ee4895f240aedeac44915d
Felix Owolabi <owolabifelix78@gmail.com>
2023-11-16 16:19:10 +0100

    react-testing

--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 const Application = () => {
   return (
+      <>
+      <h1>Welcome to React-Testing</h1>
+      <h2>Section 1</h2>
      <form>
         <div>
             <label htmlFor="name">Name</label>
             <input type="text" id='name' />
+            <label htmlFor="bio">Bio</label>
+            <input type="text" id='bio' />
         </div>
         <div>
-            <label htmlFor="job-location">Job location</label>
+            <label htmlFor="job-location">Name</label>
             <select name="" id="job-location">
                 <option value="">Select a Country</option>
                 <option value="US">United States</option>
@@ -25,6 +30,8 @@ const Application = () => {
             <button>Submit</button>
         </div>
      </form>
+     </>
+
   )
 }
 
--- src/components/application/application.spec.tsx
+++ src/components/application/application.spec.tsx
@@ -4,9 +4,18 @@ import Application from "./Application"
 describe('Application',()=>{
   test('renders correctly',()=>{
     render(<Application/>)
-    const nameElement = screen.getByRole('textbox')
-    expect(nameElement).toBeInTheDocument()
 
+    const pageElement = screen.getByRole('heading',{level:1})
+    expect(pageElement).toBeInTheDocument()
+
+    const screenElement = screen.getByRole('heading',{level:2})
+    expect(screenElement).toBeInTheDocument()
+
+    const nameElement = screen.getByRole('textbox',{name:'Name'})
+    expect(nameElement).toBeInTheDocument()
+    const nameElement2 = screen.getByLabelText('Name',{selector:'input'})
+    expect(nameElement2).toBeInTheDocument()
+    
     const joblocationElement = screen.getByRole('combobox')
     expect(joblocationElement).toBeInTheDocument()
   
