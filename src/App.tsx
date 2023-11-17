3a03be7a7f7f92e6b9c920835d1582f3c48aacd3
Felix Owolabi <owolabifelix78@gmail.com>
2023-11-17 11:03:43 +0100

    react-testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Application from './components/application/Application';
+import { Skills } from './components/skills/skills';
 
 function App() {
+  const skills = ['HTML','CSS','Javascript']
+  
   return (
     <div className="App">
      <Application/>
+     <hr />
+     <Skills skills={skills}/>
     </div>
   );
 }
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { SkillsProps } from "./skills.types";
 
 export const Skills = ( props: SkillsProps)=>{
    const { skills } = props;
+   const [ isLoggedIn,setIsLoggedIn] = useState(false);
+
    return (
     <>
       <ul>
@@ -10,6 +13,13 @@ export const Skills = ( props: SkillsProps)=>{
         })
         }
       </ul>
+      {
+        isLoggedIn ? (
+            <button>Start Learning</button>
+        ) :(
+            <button onClick={()=>setIsLoggedIn(true)}>Log In</button>
+        )
+      }
     </>
    )
 
