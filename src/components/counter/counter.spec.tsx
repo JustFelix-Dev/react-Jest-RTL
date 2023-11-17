3b7d583a8c40215aef47369e61f92c1513c09d02
Felix Owolabi <owolabifelix78@gmail.com>
2023-11-17 15:26:47 +0100

    react-testing

--- src/components/counter/counter.spec.tsx
+++ src/components/counter/counter.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import { Counter } from "./Counter"
+import user from "@testing-library/user-event"
 
 describe('Counter',()=>{
     test('renders correctly',()=>{
@@ -10,4 +11,29 @@ describe('Counter',()=>{
         const buttonElement = screen.getByRole('button',{name:'Increment'})
         expect(buttonElement).toBeInTheDocument()
     })
+
+    test('renders an initial of 0',()=>{
+        render(<Counter />)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent('0')
+    })
+    test('renders a count of 1 after clicking the Increment button',async()=>{
+        user.setup();
+        render(<Counter />)
+        const buttonElement = screen.getByRole('button',{name:'Increment'})
+       await  user.click(buttonElement)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent('1')
+
+    })
+    test('renders a count of 2 after clicking the Increment button',async()=>{
+        user.setup();
+        render(<Counter />)
+        const buttonElement = screen.getByRole('button',{name:'Increment'})
+       await  user.dblClick(buttonElement)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent('2')
+
+    })
+
 })
