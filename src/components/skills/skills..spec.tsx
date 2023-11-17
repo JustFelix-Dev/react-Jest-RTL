d6cb39eacd047cf027e4038e391954d90cce391e
Felix Owolabi <owolabifelix78@gmail.com>
2023-11-17 10:30:53 +0100

    react-testing

--- src/components/skills/skills..spec.tsx
+++ src/components/skills/skills..spec.tsx
@@ -2,10 +2,17 @@ import { render, screen } from "@testing-library/react"
 import { Skills } from "./skills"
 
 describe('Skills',()=>{
+    const skills = ['HTML','CSS','Javascript']
+
     test('renders correctly',()=>{
-        const skills = ['HTML','CSS','Javascript']
         render(<Skills skills={skills}/>)
         const listElement = screen.getByRole('list')
         expect(listElement).toBeInTheDocument()
+    })
+
+    test('renders list elements',()=>{
+        render(<Skills skills={skills}/>)
+        const listItemElements = screen.getAllByRole('listitem')
+        expect(listItemElements).toHaveLength(skills.length)
     })
 })
