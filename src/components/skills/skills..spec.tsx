2f607fc28cbc4a36e4d8b16a7998e9fa8d354da5
Felix Owolabi <owolabifelix78@gmail.com>
2023-11-17 10:17:35 +0100

    react-testing

--- src/components/skills/skills..spec.tsx
+++ src/components/skills/skills..spec.tsx
@@ -1,9 +1,11 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { Skills } from "./skills"
 
 describe('Skills',()=>{
     test('renders correctly',()=>{
         const skills = ['HTML','CSS','Javascript']
         render(<Skills skills={skills}/>)
+        const listElement = screen.getByRole('list')
+        expect(listElement).toBeInTheDocument()
     })
 })
